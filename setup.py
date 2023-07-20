f1fcf75ff8a3bec7f96ff1464a3f943a406f57e7
finn fierro <hm19194@bristol.ac.uk>
2023-07-20 11:07:23 +0100

    small change

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'web3r',
-  version = '0.0.3',  # or '0.1.1' or whatever your next version number should be
+  version = '0.0.4',  # or '0.1.1' or whatever your next version number should be
   packages=find_packages(),
   license='MIT',
   description = 'Web3R.py is a Python wrapper that enhances the functionality of Web3.py and integrates with Etherscan\'s API.',
   author = 'Finn Castro',
   author_email = 'finnfierro@gmail.com',  # Update with your email
   url = 'https://github.com/FinnCastro/Web3R',
-  download_url = 'https://github.com/FinnCastro/Web3R/archive/refs/tags/0.2.tar.gz',
+  download_url = 'https://github.com/FinnCastro/Web3R/archive/refs/tags/0.0.4.tar.gz',
   keywords = ['web3.py', 'Etherscan', 'Ethereum', 'Blockchain', 'API'],
   install_requires=[
           'web3',  # Assuming that your project depends on web3 and requests
